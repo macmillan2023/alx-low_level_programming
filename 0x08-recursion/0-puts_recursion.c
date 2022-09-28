5ebeb77e777ea12c7e93bb21e3843ea5e8fbe084
Macmillan Joseph <macmillanjoseph1@gmail.com>
2022-09-28 15:29:55 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _put_recurion - print's a string followed by a new line
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
