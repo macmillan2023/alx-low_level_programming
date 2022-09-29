f573d086b705acc11f18e76b22eec7f1f13a4a1f
Macmillan Joseph <macmillanjoseph1@gmail.com>
2022-09-28 18:03:41 -0700

    2. Dreams feel real while we are in them.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - calculates the length of a string
- * @: string to be used
- *
- * Return: length of the string
+ * _strlen_recursion - length of string
+ * @s: string to count
+ * Return: length of string
  */
+
 int _strlen_recursion(char *s)
 {
-	int sum = 0;
+	int n = 0;
 
-	if (*s != '\n')
+	if (*s)
 	{
-		sum++;
-		sum += _strlen_recursion(s + 1);
+		n = _strlen_recursion(s + 1);
+		return (n += 1);
 	}
-	return (sum);
+	return (0);
 }
