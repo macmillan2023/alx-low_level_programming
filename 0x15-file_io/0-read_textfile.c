205551ca2ee82c2c2c12894cd7abe081782f409a
Macmillan Joseph <macmillanjoseph1@gmail.com>
2022-11-01 16:08:43 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * @filename: The name of the file to read from
  * @letters: The number of characters it should print to the STDOUT
  * Return: Returns the number of characters printed
- * */
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fild = 0, chk = 0;
